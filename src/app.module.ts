cc73eb00387effc026dea48685b396d876faeede
Bartlomiej <bartlomiej.blaszczak@flip.shop>
2025-04-09 15:30:05 +0200

    feat(): cleanup - remove app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BinanceModule } from './binance/binance.module';
 import { MarketDataModule } from './market-data/market-data.module';
 
 @Module({
   imports: [BinanceModule, MarketDataModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
